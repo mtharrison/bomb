73251014c9f053ca556ef36658daa1cb790b70ab
Matt Harrison <hi@matt-harrison.com>
2015-03-18 23:23:57 +0000

    Updated examples

--- examples/express/index.js
+++ examples/express/index.js
@@ -1,10 +1,10 @@
 var Express = require('express');
-var Busta = require('../../index');
+var Bomb = require('../../index');
 var Path = require('path');
 
 var app = Express();
 
-var box = new Busta.Box({
+var box = new Bomb.Box({
     url: '/public',
     path: Path.join(__dirname, 'public'),
     regex: /\.(css|js|html)/
